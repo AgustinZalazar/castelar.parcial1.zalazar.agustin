1b562261f98802db6beeafb175c712996c875d22
Agustin <agustin.zalazar@uai.edu.ar>
2020-06-04 19:33:03 -0300

    Estructura HTML

--- castelar.parcial1.zalazar.agustin/ParcialZalazar/App_Start/RouteConfig.cs
+++ castelar.parcial1.zalazar.agustin/ParcialZalazar/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ParcialZalazar
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Cosmo", id = UrlParameter.Optional }
             );
         }
     }
